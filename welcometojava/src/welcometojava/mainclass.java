68585c569c8ba4fd402bdb969d0803745dc16cb5
venkatasai-pavanteja <pavanyerrapragada40948@gmail.com>
2022-01-12 20:30:39 +0530

    welcometojava

---
+++ welcometojava/src/welcometojava/mainclass.java
@@ -0,0 +1,16 @@
+package welcometojava;
+
+public class mainclass {
+	
+	
+	public static void string()
+	{
+		System.out.println("welcome to Java Programming");
+	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    	string();
+  
+    }
+
+} 
