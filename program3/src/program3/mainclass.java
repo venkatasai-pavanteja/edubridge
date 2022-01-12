2b1d7f02a195fc94f9e94081e43455476dbfeb92
venkatasai-pavanteja <pavanyerrapragada40948@gmail.com>
2022-01-12 21:07:27 +0530

    program3

---
+++ program3/src/program3/mainclass.java
@@ -0,0 +1,12 @@
+package program3;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		String ch= "Hello";
+		String str = "welcome";
+		System.out.println(ch);
+		System.out.println(ch+" "+ch);
+		System.out.print(str+" "+str);
+	}
+}
